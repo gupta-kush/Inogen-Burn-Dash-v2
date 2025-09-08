2da801647e506eed5f5766e16b7d34b9f575b662
Kush Gupta <54411218+gupta-kush@users.noreply.github.com>
2025-09-08 08:59:30 -0500

    Updated Rack Number Font Size

--- components/Rack.tsx
+++ components/Rack.tsx
@@ -12,7 +12,7 @@ interface RackProps {
 const Rack: React.FC<RackProps> = ({ rackNumber, shelves, onUpdateShelf }) => {
     return (
         <div className="bg-slate-100/80 dark:bg-slate-800/50 rounded-lg shadow-xl p-2 flex flex-col gap-1.5 border border-slate-200 dark:border-slate-700 flex-shrink-0 w-48">
-            <h2 className="text-sm font-bold text-center text-brand-accent-blue border-b border-slate-200 dark:border-slate-700 pb-1 mb-1">
+            <h2 className="text-base font-bold text-center text-brand-accent-blue border-b border-slate-200 dark:border-slate-700 pb-1 mb-1">
                 Rack {rackNumber}
             </h2>
             <div className="flex flex-col gap-1.5">
